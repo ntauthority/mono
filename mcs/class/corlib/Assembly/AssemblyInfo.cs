c50a91b3a6a82cb6521180af53b33893c05fef60
NTAuthority <bas@dotbas.net>
2016-09-18 12:29:07 +0200

    expose internals to the old Cfx platform binary, pending changes

--- mcs/class/corlib/Assembly/AssemblyInfo.cs
+++ mcs/class/corlib/Assembly/AssemblyInfo.cs
@@ -80,6 +80,8 @@ using System.Runtime.InteropServices;
 #else
 [assembly: InternalsVisibleTo ("System, PublicKey=00000000000000000400000000000000")]
 [assembly: InternalsVisibleTo ("System.Core, PublicKey=00000000000000000400000000000000")]
+[assembly: InternalsVisibleTo ("CitizenFX.Core, PublicKey=00000000000000000400000000000000")]
+[assembly: InternalsVisibleTo ("Mono.CSharp, PublicKey=002400000480000094000000060200000024000052534131000400000100010079159977d2d03a8e6bea7a2e74e8d1afcc93e8851974952bb480a12c9134474d04062447c37e0e68c080536fcf3c3fbe2ff9c979ce998475e506e8ce82dd5b0f350dc10e93bf2eeecf874b24770c5081dbea7447fddafa277b22de47d6ffea449674a4f9fccf84d15069089380284dbdd35f46cdff12a1bd78e4ef0065d016df")]
 #endif
 
 [assembly: Guid ("BED7F4EA-1A96-11D2-8F08-00A0C9A6186D")]
